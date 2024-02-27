2bbccf0a7b0c94b8a82bc237075f72e18b91d814
NekoCarreraDesigns <denvermaas@gmail.com>
2024-02-27 13:27:01 -0700

    added header and navbar components

---
+++ portfolio-site-five/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className='header-wrapper'>
+        <header className='page-header'>
+          Aloha, Welcome to Neko Carrera Designs!
+        </header>
+        <p className='header-paragraph'>
+          Thanks for visiting the site, you will find various projects created
+          by Neko Carrera Designs featured here. There is also some fun games to
+          play if you get bored. Do not worry I will npt tell your boss, lol
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ portfolio-site-five/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import "./Navbar.css";
+import { GiPalmTree } from "react-icons/gi";
+
+const Navbar = () => {
+  return (
+    <nav className='page-nav'>
+      <a className='nav-link' href='/'>
+        Home
+      </a>
+      {"  "}
+      <a className='nav-link' href='/projects'>
+        Projects
+      </a>
+      {"  "}
+      <a className='nav-link' href='/contact'>
+        Contact
+      </a>
+      <div className='circle'>
+        <p className='circle-text'>
+          <GiPalmTree /> Neko Carrera Designs <GiPalmTree />
+        </p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
