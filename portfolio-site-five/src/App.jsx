8ed0736da39f3cf13e11c5dd4b4d04ca3c332719
NekoCarreraDesigns <denvermaas@gmail.com>
2024-02-28 15:56:32 -0700

    removed old executable, and deleted App.css, added routes to App.jsx

--- portfolio-site-five/src/App.jsx
+++ portfolio-site-five/src/App.jsx
@@ -1,13 +1,20 @@
-import Header from './components/Header/Header'
-import Navbar from './components/Navbar/Navbar'
-import './App.css'
+import {Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar/Navbar"
+import Home from "./pages/Home/Home"
+import Contact from "./pages/Contact/Contact"
+import Projects from "./pages/Projects/Projects"
+
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/projects" element={<Projects/>}></Route>
+      </Routes>
     </>
   )
 }
